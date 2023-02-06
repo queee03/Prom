ffc916355934b2cd2be903793706ada8ca9ba5c2
yangzq <yangzq@infore.com>
2023-02-06 18:25:17 +0800

    test(Menu): 新增SubMenu单元测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,4 +1,4 @@
-import { render, RenderResult, cleanup } from '@testing-library/react';
+import { render, RenderResult, cleanup, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Menu from './index';
 import { MenuProps } from './menu';
@@ -20,9 +20,31 @@ const generateMenu = (props: MenuProps) => {
       <Menu.Item>active</Menu.Item>
       <Menu.Item disabled>disabled</Menu.Item>
       <Menu.Item>prom</Menu.Item>
+      <Menu.SubMenu title="sub">
+        <Menu.Item>sub 1</Menu.Item>
+        <Menu.Item>sub 2</Menu.Item>
+      </Menu.SubMenu>
       {/* <li>error</li> */}
     </Menu>
   );
+};
+
+// test 不会读取 css 样式，需要手动模拟
+const createStyleFile = () => {
+  const cssFile: string = `
+    .pm-submenu {
+      display: none;
+    }
+
+    .pm-submenu.is-opened {
+      display: block；
+    }
+  `;
+
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = cssFile;
+  return style;
 };
 
 let wrapper: RenderResult,
@@ -33,6 +55,7 @@ let wrapper: RenderResult,
 describe('test Menu and MenuItem component', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps));
+    wrapper.container.append(createStyleFile());
     menuElement = wrapper.getByTestId('pm-menu');
     activeElement = wrapper.getByText('active');
     disabledElement = wrapper.getByText('disabled');
@@ -41,7 +64,9 @@ describe('test Menu and MenuItem component', () => {
   test('should render the correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument();
     expect(menuElement).toHaveClass('pm-menu test');
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+    // expect(menuElement.getElementsByTagName('li').length).toEqual(4); // 6
+    // https://developer.mozilla.org/zh-CN/docs/Web/CSS/:scope
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
     expect(activeElement).toHaveClass('pm-menu-item is-active');
     expect(disabledElement).toHaveClass('pm-menu-item is-disabled');
   });
@@ -64,5 +89,22 @@ describe('test Menu and MenuItem component', () => {
     const wrapper = render(generateMenu(testVerProps));
     const menuElement = wrapper.getByTestId('pm-menu');
     expect(menuElement).toHaveClass('pm-menu-vertical');
+  });
+
+  test('should show dropdown items when hover on subMenu', async () => {
+    const querySubElement = () => wrapper.queryByText('sub 1');
+    expect(querySubElement()).not.toBeVisible();
+
+    const dropdownElement = wrapper.getByText('sub');
+    userEvent.hover(dropdownElement);
+    await waitFor(() => {
+      expect(querySubElement()).toBeVisible();
+    });
+    userEvent.click(wrapper.getByText('sub 1'));
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+    userEvent.unhover(dropdownElement);
+    await waitFor(() => {
+      expect(querySubElement()).not.toBeVisible();
+    });
   });
 });
