e34c14e4f2e735b550f28b557fd4a8d8ad934ae2
yangzq <yangzq@infore.com>
2023-03-09 11:19:21 +0800

    perf(AutoComplete): 支持受控

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -2,19 +2,39 @@ import { useState } from 'react';
 
 import { ComponentMeta } from '@storybook/react';
 
+import { AutoComplateProps } from './autoComplete';
 import AutoComplete from './index';
 
 const Com: ComponentMeta<typeof AutoComplete> = {
   title: 'AutoComplete',
   component: AutoComplete,
-  // decorators: [(Story) => <div style={{ width: 350 }}>{Story()}</div>],
+  decorators: [(Story) => <div style={{ width: 350 }}>{Story()}</div>],
 };
 export default Com;
 
 export const Default = () => {
-  const [options, setOptions] = useState<Array<{ value: string }>>([]);
+  const [options, setOptions] = useState<AutoComplateProps['options']>([]);
+
+  const mockVal = (str: string, repeat = 1) => str.repeat(repeat);
+
+  const getPanelValue = (searchText: string) =>
+    !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
+
+  return (
+    <AutoComplete
+      placeholder="input here"
+      options={options}
+      onSearch={(text) => setOptions(getPanelValue(text))}
+    />
+  );
+};
+Default.storyName = '基本使用';
+
+export const CustomOptions = () => {
+  const [options, setOptions] = useState<AutoComplateProps['options']>([]);
 
   const mockVal = (str: string, repeat = 1) => ({
+    label: <span style={{ color: '#1677ff' }}>自定义 label {str.repeat(repeat)}</span>,
     value: str.repeat(repeat),
   });
 
@@ -23,10 +43,34 @@ export const Default = () => {
 
   return (
     <AutoComplete
+      placeholder="input here"
       options={options}
       onSearch={(text) => setOptions(getPanelValue(text))}
-      placeholder="input here"
     />
   );
 };
-Default.storyName = '基本使用';
+CustomOptions.storyName = '自定义选项';
+
+export const Controlled = () => {
+  const [value, setValue] = useState<string>();
+  const [options, setOptions] = useState<AutoComplateProps['options']>([]);
+
+  const mockVal = (str: string, repeat = 1) => str.repeat(repeat);
+
+  const getPanelValue = (searchText: string) =>
+    !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
+
+  return (
+    <AutoComplete
+      placeholder="input here"
+      options={options}
+      value={value}
+      onChange={setValue}
+      onSearch={(text) => setOptions(getPanelValue(text))}
+      onSelect={(selectedValue) => {
+        setValue(`修改值 ${selectedValue}`);
+      }}
+    />
+  );
+};
+Controlled.storyName = '受控的';
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,4 +1,5 @@
-import { useState } from '@storybook/addons';
+import { useState } from 'react';
+
 import classnames from 'classnames';
 import Input from 'components/Input';
 import { InputProps } from 'components/Input/input';
@@ -10,26 +11,33 @@ export type OptionType =
       label?: string | JSX.Element;
       value: string;
     };
-export interface AutoComplateProps extends Omit<InputProps, 'onSelect'> {
+export interface AutoComplateProps extends Omit<InputProps, 'onSelect' | 'onChange'> {
   options: OptionType[];
-  onSearch: (value: string) => void;
-  onSelect?: (value: string) => void;
+  onSearch?: (value: string) => void;
+  onSelect?: (value: string, item: OptionType) => void;
+  onChange?: (value: string) => void;
 }
 
 export const AutoComplate: React.FC<AutoComplateProps> = (props) => {
-  const { options, onSearch, onSelect, onChange, value, ...restProps } = props;
-  // const [inputValue, setInputValue] = useState(value);
+  const { options, onSearch, onSelect, onChange, ...restProps } = props;
+  const [inputValue, setInputValue] = useState<string>();
   const classess = classnames(`${PM_PREFIX_CLS}-auto-complete`, {});
 
+  if (!('value' in props)) {
+    restProps.value = inputValue;
+  }
+
   const handleChange: InputProps['onChange'] = (e) => {
-    // setInputValue(e.target.value);
-    onSearch(e.target.value);
-    onChange?.(e);
+    setInputValue(e.target.value);
+    onSearch?.(e.target.value);
+    onChange?.(e.target.value);
   };
 
-  const handleSelect = (item: { value: string }) => {
-    // setInputValue(item.value);
-    onSelect?.(item.value);
+  const handleSelect: AutoComplateProps['onSelect'] = (value, item) => {
+    setInputValue(value);
+    // onChange 在 onSelect 之前，后者修改 value 的优先级更高
+    onChange?.(value);
+    onSelect?.(value, item);
   };
 
   const generateDropdown = () => {
@@ -37,9 +45,9 @@ export const AutoComplate: React.FC<AutoComplateProps> = (props) => {
       <ul>
         {options.map((item) => {
           const label = typeof item === 'string' ? item : item.label || item.value;
-          const itemValue = typeof item === 'string' ? item : item.value;
+          const value = typeof item === 'string' ? item : item.value;
           return (
-            <li key={itemValue} onClick={() => handleSelect({ value: itemValue })}>
+            <li key={value} onClick={() => handleSelect(value, item)}>
               {label}
             </li>
           );
