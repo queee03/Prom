79eee4bd26011eab084d0acb2ba432be38e99531
yangzq <yangzq@infore.com>
2023-03-09 18:10:13 +0800

    perf(AutoComplete): 支持自定义筛选；其他优化

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -12,10 +12,12 @@ const Com: ComponentMeta<typeof AutoComplete> = {
 };
 export default Com;
 
+const mockVal = (str: string, repeat = 1) => ({
+  value: str.repeat(repeat),
+});
+
 export const Default = () => {
   const [options, setOptions] = useState<AutoComplateProps['options']>([]);
-
-  const mockVal = (str: string, repeat = 1) => str.repeat(repeat);
 
   const getPanelValue = (searchText: string) =>
     !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
@@ -30,32 +32,9 @@ export const Default = () => {
 };
 Default.storyName = '基本使用';
 
-export const CustomOptions = () => {
-  const [options, setOptions] = useState<AutoComplateProps['options']>([]);
-
-  const mockVal = (str: string, repeat = 1) => ({
-    label: <span style={{ color: '#1677ff' }}>自定义 label {str.repeat(repeat)}</span>,
-    value: str.repeat(repeat),
-  });
-
-  const getPanelValue = (searchText: string) =>
-    !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
-
-  return (
-    <AutoComplete
-      placeholder="input here"
-      options={options}
-      onSearch={(text) => setOptions(getPanelValue(text))}
-    />
-  );
-};
-CustomOptions.storyName = '自定义选项';
-
 export const Controlled = () => {
   const [value, setValue] = useState<string>();
   const [options, setOptions] = useState<AutoComplateProps['options']>([]);
-
-  const mockVal = (str: string, repeat = 1) => str.repeat(repeat);
 
   const getPanelValue = (searchText: string) =>
     !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
@@ -74,3 +53,51 @@ export const Controlled = () => {
   );
 };
 Controlled.storyName = '受控的';
+
+export const CustomOptions = () => {
+  const options = [
+    {
+      label: <span style={{ color: '#1677ff' }}>自定义 label 1</span>,
+      value: '自定义 label 1',
+    },
+    {
+      label: <span style={{ color: '#0958d9' }}>自定义 label 2</span>,
+      value: '自定义 label 2',
+    },
+    {
+      label: <span style={{ color: '#003eb3' }}>自定义 label 3</span>,
+      value: '自定义 label 3',
+    },
+  ];
+
+  return <AutoComplete placeholder="input here" options={options} />;
+};
+CustomOptions.storyName = '自定义选项';
+
+export const Customfilter = () => {
+  const options = [
+    {
+      value: 'Burns Bay Road',
+    },
+    {
+      value: 'Downing Street',
+    },
+    {
+      value: 'Wall Street',
+    },
+  ];
+
+  return (
+    <>
+      <AutoComplete
+        placeholder="input here"
+        options={options}
+        filterOption={(inputValue, option) =>
+          option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+        }
+      />
+      <AutoComplete placeholder="不筛选选项" options={options} filterOption={false} />
+    </>
+  );
+};
+Customfilter.storyName = '自定义筛选';
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -5,21 +5,32 @@ import Input from 'components/Input';
 import { InputProps } from 'components/Input/input';
 import { PM_PREFIX_CLS } from 'configs/constant';
 
-export type OptionType =
-  | string
-  | {
-      label?: string | JSX.Element;
-      value: string;
-    };
+export interface OptionType {
+  label?: string | JSX.Element;
+  value: string;
+}
+export type FilterOptionFunctionType = (value: string, option: OptionType) => boolean;
 export interface AutoComplateProps extends Omit<InputProps, 'onSelect' | 'onChange'> {
   options: OptionType[];
   onSearch?: (value: string) => void;
-  onSelect?: (value: string, item: OptionType) => void;
+  onSelect?: (value: string, option: OptionType) => void;
   onChange?: (value: string) => void;
+  value?: string;
+  filterOption?: boolean | FilterOptionFunctionType;
 }
 
 export const AutoComplate: React.FC<AutoComplateProps> = (props) => {
-  const { options, onSearch, onSelect, onChange, ...restProps } = props;
+  const {
+    options,
+    onSearch,
+    onSelect,
+    onChange,
+    onFocus,
+    onBlur,
+    filterOption = true,
+    ...restProps
+  } = props;
+  const [isOpened, setIsOpened] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>();
   const classess = classnames(`${PM_PREFIX_CLS}-auto-complete`, {});
 
@@ -31,23 +42,37 @@ export const AutoComplate: React.FC<AutoComplateProps> = (props) => {
     setInputValue(e.target.value);
     onSearch?.(e.target.value);
     onChange?.(e.target.value);
+    // setIsOpened(true);
   };
 
-  const handleSelect: AutoComplateProps['onSelect'] = (value, item) => {
+  const handleSelect: AutoComplateProps['onSelect'] = (value, option) => {
     setInputValue(value);
     // onChange 在 onSelect 之前，后者修改 value 的优先级更高
     onChange?.(value);
-    onSelect?.(value, item);
+    onSelect?.(value, option);
+    setIsOpened(false);
   };
 
   const generateDropdown = () => {
+    let filter: FilterOptionFunctionType;
+    if (filterOption) {
+      if (typeof filterOption === 'boolean') {
+        filter = (_, option) => option.value.indexOf(restProps.value || '') !== -1;
+      } else {
+        filter = filterOption;
+      }
+    } else {
+      filter = () => true;
+    }
+
+    const currentOptions = options.filter((option) => filter(restProps.value || '', option));
+
     return (
       <ul>
-        {options.map((item) => {
-          const label = typeof item === 'string' ? item : item.label || item.value;
-          const value = typeof item === 'string' ? item : item.value;
+        {currentOptions.map((option) => {
+          const label = option.label || option.value;
           return (
-            <li key={value} onClick={() => handleSelect(value, item)}>
+            <li key={option.value} onClick={() => handleSelect(option.value, option)}>
               {label}
             </li>
           );
@@ -58,8 +83,15 @@ export const AutoComplate: React.FC<AutoComplateProps> = (props) => {
 
   return (
     <div className={classess}>
-      <Input onChange={handleChange} {...restProps} />
-      {generateDropdown()}
+      <Input
+        onChange={handleChange}
+        onFocus={(e) => {
+          setIsOpened(true);
+          onFocus?.(e);
+        }}
+        {...restProps}
+      />
+      {isOpened && generateDropdown()}
     </div>
   );
 };
