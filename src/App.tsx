25e61703fcb0b669e71ce42bd406e6c65fdd51b8
yangzq <yangzq@infore.com>
2023-03-15 17:55:15 +0800

    chore: 安装依赖 axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import './App.css';
+
 function App() {
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    const postData = { title: 'Prom' };
+    axios.post('https://jsonplaceholder.typicode.com/posts', postData).then((res) => {
+      setTitle(res.data.title);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">npm run storybook</header>
+      <header className="App-header">{title}</header>
+      <header className="App-header">try `npm run storybook`</header>
     </div>
   );
 }
