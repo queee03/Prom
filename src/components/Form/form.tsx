a60ca0b3b5ba5d0809d68aa05a847327de59dc8e
yangzq <yangzq@infore.com>
2023-04-11 11:57:56 +0800

    feat(Form): 新增 useStore 以支持表单数据管理

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -1,16 +1,26 @@
 import classnames from 'classnames';
 import { PM_PREFIX_CLS } from 'configs/constant';
 
+import useStore from './useStore';
+
 type OriginFormProps = React.FormHTMLAttributes<HTMLFormElement>;
 type FormProps = OriginFormProps;
 
 export const Form: React.FC<FormProps> = (props) => {
   const { children, className, ...restProps } = props;
+  const { form, fields } = useStore();
+
   const classes = classnames(`${PM_PREFIX_CLS}-form`, className);
   return (
-    <form className={classes} {...restProps}>
-      {children}
-    </form>
+    <>
+      <form className={classes} {...restProps}>
+        {children}
+      </form>
+      <div>
+        <pre>{JSON.stringify(form)}</pre>
+        <pre>{JSON.stringify(fields)}</pre>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Form/useStore.ts
@@ -0,0 +1,46 @@
+import { useReducer, useState } from 'react';
+
+export interface FieldDetail {
+  name: string;
+  value: string;
+  rules: unknown[];
+  isValid: boolean;
+  errors: any[];
+}
+
+export interface FieldState {
+  [key: string]: FieldDetail;
+}
+
+export interface FormState {
+  isValid: boolean;
+}
+
+export interface FieldsAction {
+  type: 'addField';
+  name: string;
+  value: unknown;
+}
+
+function fieldReducer(state: FormState, action: FieldsAction) {
+  switch (action.type) {
+    case 'addField':
+      return {
+        ...state,
+        [action.name]: action.value,
+      };
+    default:
+      return state;
+  }
+}
+
+function useStore() {
+  const [form, setForm] = useState<FormState>({ isValid: true });
+  const [fields, dispatch] = useReducer(fieldReducer, { isValid: true });
+  return {
+    form,
+    fields,
+    dispatch,
+  };
+}
+export default useStore;
