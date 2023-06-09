46f1e0bdaf7b3af6ad93809ab0a074f2ee9ad42d
yangzq <yangzq@infore.com>
2023-06-09 10:44:15 +0800

    chore: rollup引入判断外部依赖的插件

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
 import typescript from 'rollup-plugin-typescript2';
 
 import sass from 'rollup-plugin-sass';
@@ -11,11 +12,13 @@ export default {
     file: 'dist/index.es.js',
     format: 'es',
   },
+  // external: ['react', 'react-dom'],
   plugins: [
     // 插件是按顺序执行的
     nodeResolve(),
     commonjs(),
     json(),
+    excludeDependenciesFromBundle(),
     typescript({ tsconfig: './tsconfig.build.json' }),
     sass({
       output: 'dist/index.css',
